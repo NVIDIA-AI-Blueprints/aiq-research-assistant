bae846ea694feb83b86d6759234714a57bfaf976
Michael Demoret <mdemoret@nvidia.com>
2025-07-15 22:36:57 -0400

    Updated so CI will pass
    Signed-off-by: Michael Demoret <mdemoret@nvidia.com>

--- aira/src/aiq_aira/eval/evaluators/__init__.py
+++ aira/src/aiq_aira/eval/evaluators/__init__.py
@@ -37,5 +37,5 @@ __all__ = [
     "CitationQualityEvaluatorConfig",
     "RagasWrapperEvaluator",
     "RagasWrapperEvaluatorConfig",  # "WeaveEvaluator",
-  # "WeaveEvaluatorConfig",
+    # "WeaveEvaluatorConfig",
 ]
