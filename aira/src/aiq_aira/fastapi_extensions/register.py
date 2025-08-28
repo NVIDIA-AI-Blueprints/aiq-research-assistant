a231f46a021fd1ba2b875682be6bd783a5a56b5b
Eli Fajardo <efajardo@nvidia.com>
2025-08-28 10:27:48 -0400

    Update to custom fastapi frontend

---
+++ aira/src/aiq_aira/fastapi_extensions/register.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+FastAPI Extensions Module
+
+This module provides custom FastAPI extensions that are layered on top of the base
+AIQ/NAT server API. It includes middleware functionality such as session management
+with Redis and automatic cleanup of temporary resources.
+
+The extensions are implemented as a FastAPI plugin that can inject additional
+routes and functionality into the base FastAPI server.
+"""
+
+import os
+from typing import override
+
+from aiq.builder.workflow_builder import WorkflowBuilder
+from aiq.cli.register_workflow import register_front_end
+from aiq.data_models.config import AIQConfig
+from aiq.front_ends.fastapi.fastapi_front_end_config import FastApiFrontEndConfig
+from aiq.front_ends.fastapi.fastapi_front_end_plugin import FastApiFrontEndPlugin
+from aiq.front_ends.fastapi.fastapi_front_end_plugin_worker import FastApiFrontEndPluginWorker
+from aiq.front_ends.fastapi.fastapi_front_end_plugin_worker import FastApiFrontEndPluginWorkerBase
+from fastapi import FastAPI
+
+
+class APIExtensionsConfig(FastApiFrontEndConfig, name="aira_frontend"):
+    """Configuration for API extensions including middleware settings"""
+    pass
+
+
+class APIExtensionsWorker(FastApiFrontEndPluginWorker):
+    """Worker that adds custom API routes and middleware to the base API server"""
+
+    @override
+    async def add_routes(self, app: FastAPI, builder: WorkflowBuilder):
+        await super().add_routes(app, builder)
+
+
+class APIExtensionsPlugin(FastApiFrontEndPlugin):
+    """Plugin that extends the base API with custom functionality"""
+
+    def __init__(self, full_config: AIQConfig, config: APIExtensionsConfig):
+        super().__init__(full_config=full_config)
+        self.config = config
+
+    @override
+    def get_worker_class(self) -> type[FastApiFrontEndPluginWorkerBase]:
+        return APIExtensionsWorker
+
+
+@register_front_end(config_type=APIExtensionsConfig)
+async def register_api_extensions(config: APIExtensionsConfig, full_config: AIQConfig):
+    """Register API extensions with the AIQ framework"""
+    yield APIExtensionsPlugin(full_config=full_config, config=config)
